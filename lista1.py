ba16ced8c8ded446ef3383a53de3a48b4fd5b645
23Almeida <gabrieldealmeida2508@gmail.com>
2024-10-11 13:20:40 +0000

    q1-10

--- lista1.py
+++ lista1.py
@@ -66,20 +66,31 @@ def q10():
 #    quatro operações básicas entre estes dois números, adição,
 #    subtração,multiplicação e divisão. Ao final, o programa
 #    deve imprimir os resultados dos cálculos.
+def q11():
+    num1 = float(input('1 Número: '))
+    num2 = float(input('2 Número: '))
+    print(f'{num1} + {num2} = {round(num1+num2,2)}')
+    print(f'{num1} - {num2} = {round(num1-num2,2)}')
+    print(f'{num1} * {num2} = {round(num1*num2,2)}')
+    print(f'{num1} / {num2} = {round(num1/num2,2)}')    
 
 #12. Faça um programa que leia um número real e calcule o
 #    quadrado deste número. Ao final, o programa deve
 #    imprimir o resultado do cálculo.
 def q12():
+    numero = float(input('Número: '))
+    print(numero*numero)
+    #print(pow(numero,2))
+    #print(numero**2)
 
 #13. Faça um programa que leia o saldo de uma conta poupança e
 #    imprima o novo saldo, considerando um reajuste de 2%.
 def q13():
-    saldo = round(float(input('Saldo: R$ ')))
-    print(f'Saldo atualizado 2%: R$ {round(saldo*1 .02 ,2)})
-          
+    saldo = round(float(input('Saldo: R$ ')),2)
+    print(f'Saldo atualizado 2%: R$ {round(saldo*1.02,2)}')
+
 #14. Faça um programa que leia a base e a altura de um retângulo
-#    e imprima o perímetro (base*2 + altura*2) e a área (base * altura).
+#    e imprima o perímetro (base*2 + altura*2) e a área (base * altura).    
 
 #15. Faça um programa que leia o valor de um produto, o percentual
 #    do desconto desejado e imprima o valor do desconto e o valor
@@ -95,6 +106,10 @@ def q13():
 #    com base na fórmula a seguir. Após calcular o programa deve
 #    imprimir o resultado da conversão.
 #    F = (9 x C + 160) / 5
+def q17():
+    c = int(input('Centígrados: '))
+    f = (9 * c + 160) / 5
+    print(f'{c} C = {f} F')
 
 #18. Faça um programa que calcule a quantidade de litros de combustível
 #    consumidos em uma viagem, sabendo-se que o carro tem autonomia de
@@ -108,6 +123,13 @@ def q13():
 #    • L = Litros de combustível consumidos
 #    Ao final, o programa deverá imprimir a distância percorrida e a
 #    quantidade de litros consumidos na viagem.
+def q18():
+    tempo_decorrido = int(input('Tempo Decorrido (min): '))
+    velocidade_media = int(input('Velocidade Média (km/h): '))
+    distancia = tempo_decorrido/60 * velocidade_media
+    litros_consumidos = distancia / 12
+    print(f'Distância: {distancia}')
+    print(f'Litros Consumidos: {litros_consumidos}')
 
 #19. Faça um programa que calcule o valor de uma prestação em atraso.
 #    Para isso, o programa deve ler o valor da prestação vencida, a
@@ -115,8 +137,20 @@ def q13():
 #    programa deve imprimir o valor da prestação atrasada, o período
 #    de atraso, os juros que serão cobrados pelo período de atraso, o
 #    valor da prestação acrescido dos juros. Considere juros simples.
+def q19():
+    valor_prestacao_vencida = float(input('Valor da Prestação Vencida: R$ '))
+    taxa_juros = int(input('Taxa de Juros Diária(%): '))
+    dias_atraso = int(input('Dias de Atraso: '))
+    juros = valor_prestacao_vencida * (dias_atraso*taxa_juros/100)
+    valor_prestacao_final = valor_prestacao_vencida + juros
+    print(f'Multa por atraso: R$ {juros}')
+    print(f'Valor da Prestação Atualizada: R$ {valor_prestacao_final}')
 
 #20. Faça um programa que efetue a apresentação do valor da conversão
 #    em real (R$) de um valor lido em dólar (US$). Para isso, será
 #    necessário também ler o valor da cotação do dólar.
-q13()
+def q20():
+    dolares = round(float(input('US$: ')),2)
+    cotacao = round(float(input('Valor do dólar: R$ ')),2)
+    print(f'Qtde de Reais: R$ {round(dolares*cotacao,2)}')
+    q20()
